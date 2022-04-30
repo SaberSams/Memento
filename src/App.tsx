a08731576a102f705da77be4acc1eb8d1551b7ef
SaberSams <Stephen.sams@live.com>
2022-04-30 00:17:58 -0700

    Added CSS

--- src/App.tsx
+++ src/App.tsx
@@ -93,11 +93,11 @@ const App: React.FC = () => {
   },[cards, level])
 
   //TODO: Extract the grid to its own component
-  const gridCSS = "grid m-12 gap-2 grid-cols-" + levels[level][0]
+
   return (
     <>
       <Header wins={wins} score={score} reset={reset}></Header>
-      <div className={gridCSS}>
+      <div className={`grid m-12 gap-2 grid-cols-${levels[level][0]}`}>
         {cards.map((card) => {
           const { image, id, matched } = card;
 
